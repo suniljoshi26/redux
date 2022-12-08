8ff5fe17bde25ee97489a0aeab94c7c20ba323cf
Sunil Joshi <joshisunil2601@gmail.com>
2022-12-08 12:21:51 +0530

    sub reduser

---
+++ modetraker/src/reducers/sadReduser.ts
@@ -0,0 +1,30 @@
+import { AnyAction } from "redux";
+import { SAD_BUTTON_CLICKED } from "../Actions";
+import { Moment } from "../Store";
+
+export type sadState = {
+  sadMoment: Moment[];
+};
+export const initialSadState = {
+  sadMoment: [],
+};
+
+function SadReduser(currentSadState: sadState, action: AnyAction) {
+  switch (action.type) {
+    case SAD_BUTTON_CLICKED:
+      return {
+        ...currentSadState,
+        sadMoment: [
+          ...currentSadState.sadMoment,
+          {
+            intensity: action.payload.count,
+            when: action.payload.when,
+          },
+        ],
+      };
+    default:
+      return currentSadState;
+  }
+}
+
+export default SadReduser;
