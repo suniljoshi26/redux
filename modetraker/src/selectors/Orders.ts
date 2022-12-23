4105db673a1e2b9d38b2cc59620b397f6172bfc5
Sunil Joshi <joshisunil2601@gmail.com>
2022-12-23 09:37:12 +0530

    detail page

--- modetraker/src/selectors/Orders.ts
+++ modetraker/src/selectors/Orders.ts
@@ -1,27 +1,64 @@
+import { createSelector } from "reselect";
 import Product from "../modules/product";
 import { State } from "../Store";
+import { productMapSelector } from "./product";
 
-export const orderLoadingSelector = (state: State) => {
-  return state.orders.loading;
+export const orderStateSelector = (state: State) => {
+  return state.orders;
 };
+export const orderLoadingSelector = createSelector(
+  orderStateSelector,
+  (orderState) => {
+    return orderState.loading;
+  }
+);
 
-export const ordersSelector = (state: State) => {
-  const normalizeorders = state.orders.orders;
-  const orderArr = Object.keys(normalizeorders).map(
-    (orderId) => normalizeorders[+orderId]
-  );
-  return orderArr;
-};
-export const ordersMapSelector = (state: State) => {
-  return state.orders.orders;
-};
-export const orderProductSelector = (state: State) => {
-  return Object.keys(state.orders.orders).reduce<{
-    [productId: number]: Product[];
-  }>((previous, currentOrderId) => {
-    const order = state.orders.orders[+currentOrderId];
-    const products = order.products.map((pid) => state.products.products[pid]);
+// export const ordersMapSelector = (state: State) => {
+//   return state.orders.orders;
+// };
 
-    return { ...previous, [currentOrderId]: products };
-  }, {});
-};
+export const ordersMapSelector = createSelector(
+  orderStateSelector,
+  (orderState) => {
+    return orderState.orders;
+  }
+);
+
+export const ordersSelector = createSelector(
+  ordersMapSelector,
+  (normalizeorders) =>
+    Object.keys(normalizeorders).map((orderId) => normalizeorders[+orderId])
+);
+// export const ordersSelector = (state: State) => {
+//   const normalizeorders = state.orders.orders;
+//   const orderArr = Object.keys(normalizeorders).map(
+//     (orderId) => normalizeorders[+orderId]
+//   );
+//   return orderArr;
+// };
+
+export const orderProductSelector = createSelector(
+  ordersMapSelector,
+  productMapSelector,
+  (orderMap, productMap) => {
+    return Object.keys(orderMap).reduce<{
+      [orderId: number]: Product[];
+    }>((previous, currentOrderId) => {
+      const order = orderMap[+currentOrderId];
+      const products = order.products.map((pid) => productMap[pid]);
+
+      return { ...previous, [currentOrderId]: products };
+    }, {});
+  }
+);
+
+//    export const orderProductSelector = (state: State) => {
+//   return Object.keys(state.orders.orders).reduce<{
+//     [productId: number]: Product[];
+//   }>((previous, currentOrderId) => {
+//     const order = state.orders.orders[+currentOrderId];
+//     const products = order.products.map((pid) => state.products.products[pid]);
+
+//     return { ...previous, [currentOrderId]: products };
+//   }, {});
+// };
--- modetraker/src/selectors/product.ts
+++ modetraker/src/selectors/product.ts
@@ -1,9 +1,25 @@
 import { State } from "../Store";
+import { createSelector } from "reselect";
 
-export function productLoadingSelector(state: State) {
-  return state.products.loading;
+export function productStateSelector(state: State) {
+  return state.products;
 }
-export function productSelector(state: State) {
-  const normalizedProducts = state.products.products;
-  return Object.keys(normalizedProducts).map((pid) => normalizedProducts[+pid]);
-}
+
+export const productLoadingSelector = createSelector(
+  productStateSelector,
+  (productState) => productState.loading
+);
+export const productMapSelector = createSelector(
+  productStateSelector,
+  (productState) => productState.products
+);
+// export function productSelector(state: State) {
+//   const normalizedProducts = state.products.products;
+//   return Object.keys(normalizedProducts).map((pid) => normalizedProducts[+pid]);
+// }
+
+export const productSelector = createSelector(
+  productMapSelector,
+  (normalizedProducts) =>
+    Object.keys(normalizedProducts).map((pid) => normalizedProducts[+pid])
+);
